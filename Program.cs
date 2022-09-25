6feeec66f8ed664c79a1ddf88a4573ea31c5fddf
Austin Hacker <austin.hacker@yahoo.com>
2022-09-24 20:58:15 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿namespace ThemeParkManager
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            ThemeParkIntroduction();
+            MainMenu();
+            
+        }
+        private static void ThemeParkIntroduction()
+        {
+            Console.WriteLine("||======================================================================================||");
+            Console.WriteLine("|| $$       $$  $$$$$$$$$$  $$        $$$$$$    $$$$$$       $$     $$       $$$$$$$$$$ ||");
+            Console.WriteLine("|| $$  $$   $$  $$          $$       $$    $$  $$    $$     $$$$   $$$$      $$         ||");
+            Console.WriteLine("|| $$ $$ $$ $$  $$$$$$$$$$  $$       $$        $$    $$    $$   $$$$  $$     $$$$$$$$$$ ||");
+            Console.WriteLine("|| $$$$   $$$$  $$          $$       $$    $$  $$    $$   $$     $$    $$    $$         ||");
+            Console.WriteLine("|| $$       $$  $$$$$$$$$$  $$$$$$$$  $$$$$$    $$$$$$   $$             $$   $$$$$$$$$$ ||");
+            Console.WriteLine("||======================================================================================||");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("              Welcome to the one stop shop for all things Theme Park!                     ");
+            Console.WriteLine();
+            Console.WriteLine("Within this application, we provide you the tools you need to create your very own virtual");
+            Console.WriteLine("theme park simulation.");
+            Console.WriteLine();
+            Console.WriteLine("                           We hope you have fun!");
+        }
+        private static void MainMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Where would you like to begin?");  
+            Console.WriteLine("Please enter your selection (E.g. 1, 2, 3 etc.) Then press your enter key.");
+            Console.WriteLine();
+            Console.WriteLine("1. Load existing Park");
+            Console.WriteLine("2. Create a New Theme Park");
+            MainMenuSelection();
+        }
+        private static void MainMenuSelection()
+        {
+            string userSelection = Console.ReadLine();
+            switch (userSelection)
+            {
+                case "1":
+
+                    break;
+                case "2":
+
+                    break;
+                default:
+                    Console.WriteLine("Invaild entry, please try again.");
+                    MainMenuSelection();
+                    break;
+            }
+        }
+    }
+}
---
+++ ThemePark.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThemeParkManager
+{
+    public class ThemePark
+    {
+        
+        public ThemePark(string name, string description)
+        {
+            Name = name;
+            Description = description;  
+        }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        
+        List<Rides> rides = new List<Rides>();
+        
+    }
+}
